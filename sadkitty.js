07c9e99a73bdeb4b9aaf70b46dcbe93abc6a8f7a
Mr. Hands <mrhands31@gmail.com>
2021-08-02 15:05:49 +0200

    Get post ids from page.

--- sadkitty.js
+++ sadkitty.js
@@ -7,16 +7,15 @@ const auth = require('./auth.json');
 		headless: false,
 	});
 	const page = await browser.newPage();
-	page.on('console', (message) => {
-		console.log(message.text());
-	});
-
+	
 	console.log('Loading main page...');
 
 	await page.goto('https://onlyfans.com', {
 		waitUntil: 'domcontentloaded',
 	});
 	await page.waitForSelector('form.b-loginreg__form');
+
+	// log in using twitter
 
 	console.log('Logging in...');
 
@@ -26,4 +25,18 @@ const auth = require('./auth.json');
 	await page.type('#username_or_email', auth.username);
 	await page.type('#password', auth.password);
 	await page.click('#allow');
+
+	// wait for posts to appear
+
+	await page.waitForSelector('.user_posts');
+
+	console.log('Logged in.');
+
+	// go to media page
+
+	await page.goto('https://onlyfans.com/daintywilder/media?order=publish_date_asc', {
+		waitUntil: 'networkidle0',
+	});
+	const postIds = await page.$$eval('.user_posts .b-post', elements => elements.map(post => post.id));
+	console.log(postIds);
 })();
