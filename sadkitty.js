41fc76aba86566f9299fc26c85ffe961846d2237
Mr. Hands <mrhands31@gmail.com>
2021-08-02 16:28:53 +0200

    Attempt to scrape a single post.

--- sadkitty.js
+++ sadkitty.js
@@ -17,40 +17,62 @@ fs.exists('./storage.db', (exists) => {
 	}
 });
 
-const authors = require('./authors.json');
+// create or open database
 
 let db = new sqlite3.Database('./storage.db');
+
+db.run(`CREATE TABLE IF NOT EXISTS Author (
+	id TEXT PRIMARY KEY,
+	name TEXT NOT NULL,
+	url TEXT
+)`);
+
+db.run(`CREATE TABLE IF NOT EXISTS Post (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	author_id TEXT NOT NULL,
+	url TEXT NOT NULL,
+	description TEXT,
+	timestamp TEXT
+)`);
+
+// load authors
+
+const authors = require('./authors.json');
 db.serialize(() => {
-	db.run(`CREATE TABLE IF NOT EXISTS Author (
-		id TEXT PRIMARY KEY,
-		name TEXT NOT NULL,
-		url TEXT
-	)`);
-
-	db.run(`CREATE TABLE IF NOT EXISTS Post (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		author_id TEXT NOT NULL,
-		url TEXT NOT NULL,
-		description TEXT,
-		timestamp TEXT
-	)`);
-
 	authors.forEach(author => {
 		db.run(`INSERT OR IGNORE INTO Author (id, name, url) VALUES (?, ?, ?)`, [author.id, author.name, `https://onlyfans.com/${author.id}`]);
 	});
-
-	db.all('SELECT * FROM Author', [], (_err, rows) => {
-		rows.forEach((row) => {
-			console.log(`name: ${row.name}`);
-		});
-	});
 });
-
-db.close();
 
 // scraping
 
-/*(async () => {
+async function scrapePost(page, url) {
+	await page.goto(url, {
+		waitUntil: 'domcontentloaded',
+	});
+
+	await page.waitForSelector('.b-post__wrapper');
+
+	const description = await page.$eval('.b-post__text-el', (element) => element.innerText);
+	const date = await page.$eval('.b-post__date > span', (element) => element.innerText);
+	const imageSource = await page.$eval('.img-responsive', (element) => element.getAttribute('src'));
+
+	console.log(description);
+	console.log(date);
+	console.log(imageSource);
+}
+
+async function scrapeMediaPage(page, author) {
+	// go to media page
+
+	await page.goto(`https://onlyfans.com/${author.id}/media?order=publish_date_asc`, {
+		waitUntil: 'networkidle0',
+	});
+	const postIds = await page.$$eval('.user_posts .b-post', elements => elements.map(post => post.id.match(/postId_(.+)/g))[1]);
+	console.log(postIds);
+}
+
+async function scrape() {
 	const browser = await puppeteer.launch({
 		headless: false,
 	});
@@ -80,11 +102,16 @@ db.close();
 
 	console.log('Logged in.');
 
-	// go to media page
+	// scrape media pages
 
-	await page.goto('https://onlyfans.com/daintywilder/media?order=publish_date_asc', {
-		waitUntil: 'networkidle0',
-	});
-	const postIds = await page.$$eval('.user_posts .b-post', elements => elements.map(post => post.id));
-	console.log(postIds);
-})();*/
+	/*db.all('SELECT * FROM Author', [], (_err, rows) => {
+		rows.forEach((row) => {
+			const author = Object.assign({}, row);
+			await scrapeMediaPage(page, author);
+		});
+	});*/
+	await scrapePost(page, 'https://onlyfans.com/176755052/daintywilder');
+}
+scrape();
+
+db.close();
